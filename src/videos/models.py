6bed7448f084ce016156d180eea15a8dc71a2afd
satvir <satvirsingh.juc@gmail.com>
2021-06-21 17:05:53 +0530

    Proxy model for Video

--- src/videos/models.py
+++ src/videos/models.py
@@ -3,9 +3,19 @@ from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(blank=True, null=True)
     description = models.TextField()
     video_id = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class VideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
 
     def __str__(self):
         return self.title
--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
-from .models import Video
+from .models import Video, VideoProxy
 
 
 class TestVideoModel(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.data1 = Video.objects.create(title='django-tutorial')
+        cls.data2 = VideoProxy.objects.create(title='django-tutorial')
 
     def test_video_model_return(self):
         """
@@ -15,3 +16,12 @@ class TestVideoModel(TestCase):
         data1 = self.data1
         self.assertIsInstance(data1, Video)
         self.assertEqual(str(data1), 'django-tutorial')
+
+    def test_video_proxy_model_return(self):
+        """
+        Test VideoProxy model return name
+        """
+        data1 = self.data1
+        data2 = self.data2
+        self.assertIsInstance(data2, VideoProxy)
+        self.assertEqual(str(data1), str(data2))
