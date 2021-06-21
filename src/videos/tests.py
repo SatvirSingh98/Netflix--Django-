d17b5a9485d74027324cdee4b1f080c263388981
satvir <satvirsingh.juc@gmail.com>
2021-06-21 16:04:08 +0530

    test for model Video using coverage module

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Video
+
+
+class TestVideoModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.data1 = Video.objects.create(title='django-tutorial')
+
+    def test_video_model_return(self):
+        """
+        Test Video model return name
+        """
+        data1 = self.data1
+        self.assertIsInstance(data1, Video)
+        self.assertEqual(str(data1), 'django-tutorial')
