dc0a40a7b06070f92c763cc4c8729fae0f6f1029
satvir <satvirsingh.juc@gmail.com>
2021-06-22 20:03:08 +0530

    Created playlists app and test cases

---
+++ src/playlists/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils import timezone
+
+from core.db.models import PublishStateOptions
+from core.db.receivers import publish_state_pre_save, slugify_pre_save
+from videos.models import Video
+
+
+class PlaylistQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(state=PublishStateOptions.PUBLISH, publish_timestamp__lte=now)
+
+
+class PlaylistManager(models.Manager):
+    def get_queryset(self):
+        return PlaylistQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+
+class Playlist(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(blank=True, null=True)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+    state = models.CharField(max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = PlaylistManager()
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def is_published(self):
+        if self.active:
+            return 'Yes'
+        return 'No'
+
+
+pre_save.connect(publish_state_pre_save, sender=Playlist)
+pre_save.connect(slugify_pre_save, sender=Playlist)
---
+++ src/playlists/tests.py
@@ -0,0 +1,67 @@
+from django.test import TestCase
+from django.utils import timezone
+from django.utils.text import slugify
+
+from videos.models import Video
+
+from .models import Playlist, PublishStateOptions
+
+
+class TestPlaylistModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.data1 = Video.objects.create(title='django-tutorial', video_id='abc123')
+        cls.data2 = Playlist.objects.create(title='django-tutorial', video=cls.data1)
+
+    def test_playlist_model_return(self):
+        """
+        Test Playlist model return name
+        """
+        data2 = self.data2
+        self.assertIsInstance(data2, Playlist)
+        self.assertEqual(str(data2), 'django-tutorial')
+
+    def test_playlist_publish_timestamp_if_published(self):
+        """
+        Test Playlist model publish_timestamp
+        """
+        data2 = self.data2
+        data2.state = PublishStateOptions.PUBLISH
+        data2.save()
+        data2.publish_timestamp = timezone.now()
+        condition = Playlist.objects.published().exists()
+        self.assertTrue(condition)
+
+    def test_playlist_publish_timestamp_if_draft(self):
+        """
+        Test Playlist model publish_timestamp
+        """
+        data2 = self.data2
+        data2.state = PublishStateOptions.DRAFT
+        data2.save()
+        self.assertEqual(data2.publish_timestamp, None)
+
+    def test_playlist_is_published(self):
+        """
+        Test Playlist model is_published
+        """
+        data2 = self.data2
+        setattr(data2, data2.is_published, 'Yes')
+        self.assertEqual(data2.is_published, 'Yes')
+        data2.active = False
+        setattr(data2, data2.is_published, 'No')
+        self.assertEqual(data2.is_published, 'No')
+
+    def test_playlist_slug_field(self):
+        """
+        Test Playlist model slug field
+        """
+        title_slug = slugify(self.data2.title)
+        self.assertEqual(title_slug, self.data2.slug)
+
+    def test_video_playlist(self):
+        """
+        Test foreign key relation
+        """
+        qs = self.data1.playlist_set.all()
+        self.assertEqual(qs.count(), 1)
